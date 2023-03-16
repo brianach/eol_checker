5ade90983265e714594d46e888d1af176379502a
brianach <brianocualain@outlook.ie>
2023-03-16 19:52:24 +0000

    Add function to write to spreadsheet

--- inv.py
+++ inv.py
@@ -24,30 +24,42 @@ def make_inv_list():
     Function to generate inventory of data entered using random ordered dating
     """
 
-    for y_r in range(4, -1, -1):
+    for y_r in range(0, -1, -1):
 
-        dhr, usr, lap, scrn, dok, key, mou, phn, hir = ([] for l_i in range(9))
+        dh, usr, lap, scr, dok, key, mou, phn, hir = ([] for l_i in range(9))
 
-        for d_n in range(1, 1+USERS//5):
+        for d in range(1, 1+USERS//5):
             rand_days = random.randrange(1, 365)
             sdate = datetime(2022, 12, 30) - timedelta((365*(y_r)+rand_days))
-            dhr.append(sdate.strftime("%d%m%Y"))
-            hir = sorted(dhr, key=lambda hird: (hird[2:4], hird[0:2]))
-        d_n = 0
+            dh.append(sdate.strftime("%d%m%Y"))
+            hir = sorted(dh, key=lambda hird: (hird[2:4], hird[0:2]))
+        p = 0
 
-        for hird in hir:
+        for h in hir:   
+            g_row = []
 
-            d_n += 1
-            usr.append(inv_heads[0][0].capitalize()+str(d_n).zfill(3)+hird)
-            lap.append(inv_heads[1][0].capitalize()+str(d_n).zfill(3)+hird)
-            scrn.append(inv_heads[2][0].capitalize()+str(d_n).zfill(3)+hird)
-            dok.append(inv_heads[3][0].capitalize()+str(d_n).zfill(3)+hird)
-            key.append(inv_heads[4][0].capitalize()+str(d_n).zfill(3)+hird)
-            mou.append(inv_heads[5][0].capitalize()+str(d_n).zfill(3)+hird)
-            phn.append(inv_heads[6][0].capitalize()+str(d_n).zfill(3)+hird)
+            usr.append(inv_heads[0][0].capitalize()+str(p).zfill(3)+h)
+            lap.append(inv_heads[1][0].capitalize()+str(p).zfill(3)+h)
+            scr.append(inv_heads[2][0].capitalize()+str(p).zfill(3)+h)
+            dok.append(inv_heads[3][0].capitalize()+str(p).zfill(3)+h)
+            key.append(inv_heads[4][0].capitalize()+str(p).zfill(3)+h)
+            mou.append(inv_heads[5][0].capitalize()+str(p).zfill(3)+h)
+            phn.append(inv_heads[6][0].capitalize()+str(p).zfill(3)+h)
 
-        print(f"{usr}\n{lap}\n{scrn}\n{dok}")
-        print(f"{key}\n{mou}\n{phn}\n{hir}")
+            g_row = usr[p], lap[p], scr[p], dok[p], key[p], mou[p], phn[p], hir[p]
+            update_inventory(g_row)
+            p += 1
+
+        print(f"{usr}\n{lap}\n{scr}\n{dok}\n{key}\n{mou}\n{phn}\n{hir}")  # test output to console
+
+
+def update_inventory(g_row):
+    """
+    Function to add data to google spreadsheet
+    """
+    inventory_worksheet = SHEET.worksheet("hardware")
+
+    inventory_worksheet.append_row(g_row)
 
 
 make_inv_list()
