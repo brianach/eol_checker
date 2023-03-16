0cee0225f76a8382429fd49b84d7974a9e523339
brianach <brianocualain@outlook.ie>
2023-03-16 21:49:44 +0000

    Clean up of code using linter

--- inv.py
+++ inv.py
@@ -15,7 +15,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("hw_inventory")
 HARDWARE = SHEET.worksheet("hardware")
 
-HARDWARE.batch_clear(["A2:H60"])
+HARDWARE.batch_clear(["A2:H60"])  # clear the worksheet to run simulation
 inv_heads = SHEET.worksheet("hardware").row_values(1)
 
 USERS = 50  # notional number of employees
@@ -23,37 +23,41 @@ inventory = []
 
 
 def make_inv_list():
-    """
+    """+str
     Function to generate inventory of data entered using random ordered dating
     """
 
-    for y_r in range(0, -1, -1):
+    for year in range(0, -1, -1):
 
-        dh, usr, lap, scr, dok, key, mou, phn, hir = ([] for l_i in range(9))
+        dthire, userid, laptop, screen, dockst, keybrd, mouses, phones, \
+            hiredt = ([] for l_i in range(9))
 
-        for d in range(1, 1+USERS//5):
+        for day in range(1, 1+USERS//5):
             rand_days = random.randrange(1, 365)
-            sdate = datetime(2022, 12, 30) - timedelta((365*(y_r)+rand_days))
-            dh.append(sdate.strftime("%d%m%Y"))
-            hir = sorted(dh, key=lambda hird: (hird[2:4], hird[0:2]))
-        p = 0
+            sdate = datetime(2022, 12, 30) - timedelta((365*(year)+rand_days))
+            dthire.append(sdate.strftime("%d%m%Y"))
+            hiredt = sorted(dthire, key=lambda hird: (hird[2:4], hird[0:2]))
 
-        for h in hir:   
+        pos = 0  # position of item in list
+
+        for dat in hiredt:
             g_row = []
 
-            usr.append(inv_heads[0][0].capitalize()+str(p).zfill(3)+h)
-            lap.append(inv_heads[1][0].capitalize()+str(p).zfill(3)+h)
-            scr.append(inv_heads[2][0].capitalize()+str(p).zfill(3)+h)
-            dok.append(inv_heads[3][0].capitalize()+str(p).zfill(3)+h)
-            key.append(inv_heads[4][0].capitalize()+str(p).zfill(3)+h)
-            mou.append(inv_heads[5][0].capitalize()+str(p).zfill(3)+h)
-            phn.append(inv_heads[6][0].capitalize()+str(p).zfill(3)+h)
+            userid.append(inv_heads[0][0].capitalize()+str(pos).zfill(3)+dat)
+            laptop.append(inv_heads[1][0].capitalize()+str(pos).zfill(3)+dat)
+            screen.append(inv_heads[2][0].capitalize()+str(pos).zfill(3)+dat)
+            dockst.append(inv_heads[3][0].capitalize()+str(pos).zfill(3)+dat)
+            keybrd.append(inv_heads[4][0].capitalize()+str(pos).zfill(3)+dat)
+            mouses.append(inv_heads[5][0].capitalize()+str(pos).zfill(3)+dat)
+            phones.append(inv_heads[6][0].capitalize()+str(pos).zfill(3)+dat)
 
-            g_row = usr[p], lap[p], scr[p], dok[p], key[p], mou[p], phn[p], hir[p]
+            g_row = userid[pos], laptop[pos], screen[pos], dockst[pos], \
+                keybrd[pos], mouses[pos], phones[pos], hiredt[pos]
             update_inventory(g_row)
-            p += 1
+            pos += 1
 
-        print(f"{usr}\n{lap}\n{scr}\n{dok}\n{key}\n{mou}\n{phn}\n{hir}")  # test output to console
+        print(f"{userid}\n{laptop}\n{screen}\n{dockst}\n{keybrd}\n{mouses}\n \
+            {phones}\n{hiredt}")  # test output to console
 
 
 def update_inventory(g_row):
