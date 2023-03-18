c84c14b8305f4eff13ed912c62fbd6daa47fef76
brianach <brianocualain@outlook.ie>
2023-03-18 17:14:42 +0000

    Store the deleted items in memory

--- inv.py
+++ inv.py
@@ -20,6 +20,11 @@ inv_heads = SHEET.worksheet("hardware").row_values(1)
 
 USERS = 50  # notional number of employees
 
+USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE = ([] for l_i in range(8))  
+INVENTORY = [USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE]
+
+UMEM, LMEM, SMEM, DMEM, KMEM, MMEM, PMEM, HMEM = ([] for l_i in range(8))
+INV_MEM = [UMEM, LMEM, SMEM, DMEM, KMEM, MMEM, PMEM, HMEM]
 
 def make_inv_list():
     """
@@ -28,40 +33,33 @@ def make_inv_list():
 
     for year in range(0, -1, -1):
 
-        userid, laptop, screen, dockst, keybrd, mouses, phones, \
-            hiredt = ([] for l_i in range(8))  # initialize lists for each variable
-        inventory = [userid, laptop, screen, dockst, keybrd, \
-            mouses, phones, hiredt]
         dthire = []
 
         for day in range(1, 1+USERS//5):
-            rand_days = random.randrange(1, 365)  # create a random number from the days in a year
+            rand_days = random.randrange(1, 365)  # create a random number 
             sdate = datetime(2022, 12, 30) - timedelta((365*(year)+rand_days))  # generate a random date
-            dthire.append(sdate.strftime("%d%m%Y"))  # add the random date to the dthire list
-            hiredt = sorted(dthire, key=lambda hird: (hird[2:4], hird[0:2]))  # oder the dthire list and copy to hiredt
+            dthire.append(sdate.strftime("%d%m%Y"))  # add random date to dthire list
+            HIRE = sorted(dthire, key=lambda hird: (hird[2:4], hird[0:2])) 
 
         pos = 0  # position of item in list
 
-        for dat in hiredt:
+        for dat in HIRE:
             i_list = 0
             g_row = []
-            for i_list in range(len(inventory)-1):
-                inventory[i_list].append(inv_heads[i_list][0].capitalize()+str(pos).zfill(3)+dat)
-                g_row.append(inventory[i_list][pos])
-            g_row.append(dat)
-            #userid[pos], laptop[pos], screen[pos], dockst[pos], \
-            #    keybrd[pos], mouses[pos], phones[pos], hiredt[pos]
+            for i_list in range(len(INVENTORY)-1):
+                INVENTORY[i_list].append(inv_heads[i_list][0].capitalize()+str(pos).zfill(3)+dat)
+                g_row.append(INVENTORY[i_list][pos])  # populate the inventory 
+            g_row.append(dat)  # add the date field
             update_inventory(g_row)
             pos += 1
 
-        #print(f"{userid}\n{laptop}\n{screen}\n{dockst}\n{keybrd}\n{mouses}\n{phones}\n{hiredt}")  # test output to console
         count = 0
-        for count in range(len(inventory)-1):
-            rand_change = random.randrange(1, 4)  # select random number of elements < 4 from list
-            remove_items =  random.sample(inventory[count], rand_change)
-            inventory[count] = [ r for r in inventory[count] if r not in remove_items]
-            print(inventory[count])
-            #print(f"Count is : {count} {random.sample(inventory[count], rand_change)}")
+        for count in range(len(INVENTORY)-1):
+            rand_change = random.randrange(1, 3)  # select random elements < 3 from list
+            remove_items =  random.sample(INVENTORY[count], rand_change)
+            INV_MEM[count].append(remove_items)  # add the removal values to a list 
+            INVENTORY[count] = [ r for r in INVENTORY[count] if r not in remove_items]  
+            print(INVENTORY[count])
 
 
 def update_inventory(g_row):
