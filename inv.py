ea9e6b8e3869121dcc7c9557ec87c90967084b38
brianach <brianocualain@outlook.ie>
2023-03-19 22:27:28 +0000

    Feat: add fucttion to check eol values

--- inv.py
+++ inv.py
@@ -20,25 +20,27 @@ inv_heads = SHEET.worksheet("hardware").row_values(1)
 
 USERS = 50  # notional number of employees
 
-USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE = ([] for l_i in range(8))  
+USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE = ([] for l_i in range(8))
 INVENTORY = [USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE]
 
 UMEM, LMEM, SMEM, DMEM, KMEM, MMEM, PMEM, HMEM = ([] for l_i in range(8))
 INV_MEM = [UMEM, LMEM, SMEM, DMEM, KMEM, MMEM, PMEM, HMEM]
 
+
 def make_inv_list():
     """
     Function to generate inventory of data entered using random ordered dating
     """
-    for year in range(2, -1, -1):
+    for year in reversed(range(4)):
 
         dthire = []
 
-        for day in range(1, 1+USERS//10):
-            rand_days = random.randrange(1, 365)  # create a random number 
-            sdate = datetime(2022, 12, 30) - timedelta((365*(year)+rand_days))  # generate a random date
-            dthire.append(sdate.strftime("%d%m%Y"))  # add random date to dthire 
-            HIRE = sorted(dthire, key=lambda hird: (hird[2:4], hird[0:2])) 
+        for day in range(1, 1+USERS//5):
+            rand_days = random.randrange(1, 365)  # create a random number
+            # generate a random date
+            sdate = datetime(2022, 12, 30) - timedelta((365*(year)+rand_days))
+            dthire.append(sdate.strftime("%d%m%Y"))  # add random date
+            HIRE = sorted(dthire, key=lambda hird: (hird[2:4], hird[0:2]))
 
         pos = 0  # position of item in list
         for dat in HIRE:
@@ -49,21 +51,28 @@ def make_inv_list():
 
         i_list = 0
         for i_list in range(len(INVENTORY)-1):
-            rand_change = random.randrange(1, 3)  # select random elements from list
-            remove_items =  random.sample(INVENTORY[i_list], rand_change)
-            for r in range(len(remove_items)):
-                INV_MEM[i_list].append(remove_items[r])  # add the removal values to a list 
-            
-        simulate_changes(year)
+            # this loop generates random items to simulate real world 
+            # replacement of employees or hardware based on real world data
+            rand_change = random.randrange(1, 3)  # select random number of items
+            remove_items = random.sample(INVENTORY[i_list], rand_change)
+            for r in remove_items:
+                # add the removal values to a list for checking against the
+                # associated inventory list eg: LAPT[] : LMEM[]
+                INV_MEM[i_list].append(r) 
+
+        simulate_changes(year)  # call the function for the given year
+
+        simulate_eol_replacement(year)  # simulate replacement of aged hardware
 
         pos = 0  # position of item in list
         for u in USER:
             g_row = []
             for i_list in range(len(INVENTORY)-1):
-                g_row.append(INVENTORY[i_list][pos])  # populate the inventory 
+                g_row.append(INVENTORY[i_list][pos])  # populate the inventory
             g_row.append(u[-8:])  # add the date field
             update_inventory(g_row)
             pos += 1
+
         update_inventory(g_row)
 
 
@@ -78,31 +87,47 @@ def update_inventory(g_row):
 
 def simulate_changes(year):
     """
-    this function generates new data to add to the inventory
+    This function simulates users leaving and hardware replacement due to failure.
+    It removes the associated item from the list and appends the new replacements.
     """
     i_list = 0  # counter for inventory current list
     for i_list in range(len(INVENTORY)-1):
         list_i = 0  # counter for inventory current list current list item
-        # at this point we want to remove only the current list values
         for i in INVENTORY[i_list]:
             r = 0  # counter for inventory memory if removal list item
-
+            # this next loop checks the values in the random replacement lists
+            # against the original list type. eg: LAPT[] : LMEM[]
             for r in range(len(INV_MEM[i_list])):
                 comp_string = INV_MEM[i_list][r][-8:]
                 inv_lst_item = i[-8:]
 
                 if comp_string == inv_lst_item:
-
+                    # if a match is found the orignal item is removed from the list
+                    # then the new item gets an incremented ID and the item is appended
                     del_date = datetime.strptime(comp_string, "%d%m%Y")
                     year_day = del_date.strftime("%j")
                     rand_days = random.randrange(1, int(year_day))
-                    new_date_str = datetime(2022, 12, 30) - timedelta((365*(year)+rand_days))
+                    new_date_str = datetime(2022, 12, 30)-timedelta((365*(year)+rand_days))
                     new_date = new_date_str.strftime("%d%m%Y")
-                    new_string = i[:1]+str((year + 1) * 10 + 1 + r ).zfill(3)+new_date
+                    new_string = i[:1]+str((year + 1)*10+1+r).zfill(3)+new_date
                     INVENTORY[i_list].pop(list_i)
                     INVENTORY[i_list].append(new_string)
                 comp_string = ""  # reinitialise the string once used
         list_i += 1
 
 
+def simulate_eol_replacement(year):
+    """
+    This function checks the type of hardware against an eol factor as follows:
+    5(years) for screens, 4(y) for laptops & docks, 3(y) for keyboard
+     & mouse and 2(y) for phones starting with the shortest eol factor
+    """
+    if year > 0:
+        if year % 2 == 0:
+            print(f"Year {year} is an even year")
+        else:
+            print(f"Year {year} is an odd year")
+
+
 make_inv_list()
+
