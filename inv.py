f8139193eb381af0c75a93536f5792cfbe19543d
brianach <brianocualain@outlook.ie>
2023-03-20 17:47:00 +0000

    Fix: resolve bug where list id's were repeating each year

--- inv.py
+++ inv.py
@@ -18,7 +18,7 @@ HARDWARE = SHEET.worksheet("hardware")
 HARDWARE.batch_clear(["A2:H60"])  # clear the worksheet to run simulation
 inv_heads = SHEET.worksheet("hardware").row_values(1)
 
-USERS = 20  # notional number of employees
+USERS = 25  # notional number of employees
 
 USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE = ([] for l_i in range(8))
 INVENTORY = [USER, LAPT, SCRN, DOCK, KEYB, MOUS, PHON, HIRE]
@@ -31,8 +31,10 @@ def make_inv_list():
     """
     Function to generate inventory of data entered using random ordered dating
     """
-    for year in reversed(range(4)):
+    id_count = 1
 
+    for year in reversed(range(4)):
+  
         dthire = []
 
         for day in range(1, 1+USERS//5):
@@ -41,13 +43,18 @@ def make_inv_list():
             sdate = datetime(2022, 12, 30) - timedelta((365*(year)+rand_days))
             dthire.append(sdate.strftime("%d%m%Y"))  # add random date
             HIRE = sorted(dthire, key=lambda hird: (hird[2:4], hird[0:2]))
+        #print(HIRE)
 
-        pos = 0  # position of item in list
         for dat in HIRE:
             i_list = 0
             for i_list in range(len(INVENTORY)-1):
-                INVENTORY[i_list].append(inv_heads[i_list][0].capitalize()+str(pos).zfill(3)+dat)
-            pos += 1
+                INVENTORY[i_list].append(inv_heads[i_list][0].capitalize()+str(id_count).zfill(3)+dat)
+            id_count += 1
+            
+        for i_list in INVENTORY:
+            print(i_list)
+        
+    for year in reversed(range(4)):
 
         i_list = 0
         for i_list in range(len(INVENTORY)-1):
@@ -58,9 +65,12 @@ def make_inv_list():
             for r in remove_items:
                 # add the removal values to a list for checking against the
                 # associated inventory list eg: LAPT[] : LMEM[]
-                INV_MEM[i_list].append(r) 
+                INV_MEM[i_list].append(r)
 
-        simulate_changes(year)  # call the function for the given year
+        #for i_list in INV_MEM:
+        #    print(i_list)   
+
+        #simulate_changes()  # call the function for the given year
 
         simulate_eol_replacement(year)  # simulate replacement of aged hardware
 
@@ -70,50 +80,58 @@ def make_inv_list():
             for i_list in range(len(INVENTORY)-1):
                 g_row.append(INVENTORY[i_list][pos])  # populate the inventory
             g_row.append(u[-8:])  # add the date field
-            update_inventory(g_row)
+            #print(f"This is year {year} and the data is {g_row}")
+            #update_inventory(g_row)
             pos += 1
 
-        update_inventory(g_row)
+        # update_inventory(g_row)
+            #print(f"This is year {year} and the data is {g_row}")
 
 
 def update_inventory(g_row):
     """
-    Function to add data to google spreadsheet
+    #Function to add data to google spreadsheet
     """
     inventory_worksheet = SHEET.worksheet("hardware")
 
     inventory_worksheet.append_row(g_row)
 
 
-def simulate_changes(year):
+
+def simulate_changes():
     """
     This function simulates users leaving and hardware replacement due to failure.
     It removes the associated item from the list and appends the new replacements.
     """
-    i_list = 0  # counter for inventory current list
-    for i_list in range(len(INVENTORY)-1):
-        list_i = 0  # counter for inventory current list current list item
-        for i in INVENTORY[i_list]:
-            r = 0  # counter for inventory memory if removal list item
-            # this next loop checks the values in the random replacement lists
-            # against the original list type. eg: LAPT[] : LMEM[]
-            for r in range(len(INV_MEM[i_list])):
-                comp_string = INV_MEM[i_list][r][-8:]
-                inv_lst_item = i[-8:]
+    for year in reversed(range(4)):
 
-                if comp_string == inv_lst_item:
-                    # if a match is found the orignal item is removed from the list
-                    # then the new item gets an incremented ID and the item is appended
-                    del_date = datetime.strptime(comp_string, "%d%m%Y")
-                    year_day = del_date.strftime("%j")
-                    rand_days = random.randrange(1, int(year_day))
-                    new_date_str = datetime(2022, 12, 30)-timedelta((365*(year)+rand_days))
-                    new_date = new_date_str.strftime("%d%m%Y")
-                    new_string = i[:1]+str((year + 1)*10+1+r).zfill(3)+new_date
-                    INVENTORY[i_list].pop(list_i)
-                    INVENTORY[i_list].append(new_string)
-                comp_string = ""  # reinitialise the string once used
-        list_i += 1
+        i_list = 0  # counter for inventory current list
+        for i_list in range(len(INVENTORY)-1):
+            list_i = 0  # counter for inventory current list current list item
+            for i in INVENTORY[i_list]:
+                r = 0  # counter for inventory memory if removal list item
+                # this next loop checks the values in the random replacement lists
+                # against the original list type. eg: LAPT[] : LMEM[]
+                for r in range(len(INV_MEM[i_list])):
+                    comp_string = INV_MEM[i_list][r][-8:]
+                    inv_lst_item = i[-8:]
+
+                    if comp_string == inv_lst_item:
+                        # if a match is found the orignal item is removed from the list
+                        # then the new item gets an incremented ID and the item is appended
+                        del_date = datetime.strptime(comp_string, "%d%m%Y")
+                        year_day = del_date.strftime("%j")
+                        rand_days = random.randrange(1, int(year_day))
+                        new_date_str = datetime(2022, 12, 30)-timedelta((365*(year)+rand_days))
+                        new_date = new_date_str.strftime("%d%m%Y")
+                        new_string = i[:1]+str((year + 1)*10+1+r).zfill(3)+new_date
+                        INVENTORY[i_list].pop(list_i)
+                        INVENTORY[i_list].append(new_string)
+                    comp_string = ""  # reinitialise the string once used
+            list_i += 1
+
+        for i_list in INVENTORY:
+            print(i_list)
 
 
 def simulate_eol_replacement(year):
