98495a0731e20229809b85bbfb33dd6ea4f14e67
brianach <brianocualain@outlook.ie>
2023-03-22 20:54:39 +0000

    Feat: add code to generate random changes to inventory

--- inv.py
+++ inv.py
@@ -92,35 +92,35 @@ def simulate_changes():
     This function takes the generate_change_list() results and removes
     the matching items from the existing lists
     """
-    for year in reversed(range(5)):
 
-        i_list = 0  # counter for inventory current list
-        for i_list in range(len(INVENTORY)-1):
-            list_i = 0  # counter for inventory current list current list item
-            for i in INVENTORY[i_list]:
-                r = 0  # counter for inventory memory if removal list item
-                # this next loop checks the values in the random replacement lists
-                # against the original list type. eg: LAPT[] : LMEM[]
-                for r in range(len(INV_MEM[i_list])):
-                    comp_string = INV_MEM[i_list][r][-8:]
-                    inv_lst_item = i[-8:]
+    # TEMPORARY CODE TO TEST INVENTORY DATA
+    # -------------------------------------
+    #with open("data.txt", mode = "a") as file:
+    #    for i_list in INVENTORY:
+    #        file.write(f"{i_list}\n")
 
-                    if comp_string == inv_lst_item:
-                        # if a match is found the orignal item is removed from the list
-                        # then the new item gets an incremented ID and the item is appended
-                        del_date = datetime.strptime(comp_string, "%d%m%Y")
-                        year_day = del_date.strftime("%j")
-                        rand_days = random.randrange(1, int(year_day))
-                        new_date_str = datetime(2022, 12, 30)-timedelta((365*(year)+rand_days))
-                        new_date = new_date_str.strftime("%d%m%Y")
-                        new_string = i[:1]+str((year + 1)*10+1+r).zfill(3)+new_date
-                        INVENTORY[i_list].pop(list_i)
-                        INVENTORY[i_list].append(new_string)
-                    comp_string = ""  # reinitialise the string once used
-            list_i += 1
+    #for year in reversed(range(5, 0, -1)):
+    year = 4
+    for id_n, s_list_2 in enumerate(INV_MEM[:-1]):
+        #match id_n = []
+        for i_1, item_2 in enumerate(s_list_2):
+            s_list_1 = INVENTORY[id_n]
+            for i_2, item_1 in enumerate(s_list_1):
+                if item_2 == item_1:
+                    pos = i_2
+                    comp_string = item_1[-8:]
+                    del_date = datetime.strptime(comp_string, "%d%m%Y")
+                    year_day = del_date.strftime("%j")
+                    rand_days = random.randrange(1, int(year_day))
+                    new_date_str = datetime(2022, 12, 30)-timedelta((365*(year)+rand_days))
+                    new_date = new_date_str.strftime("%d%m%Y")
+                    new_string = item_1[:1]+str(year*10+1).zfill(3)+new_date
+                    s_list_1.pop(pos)
+                    s_list_1.append(new_string)
 
-        for i_list in INVENTORY:
-            print(i_list)
+    #with open("data.txt", mode = "a") as file:
+    #    for i_list in INVENTORY:
+    #        file.write(f"{i_list}\n")
 
 
 def simulate_eol_replacement(year):
@@ -142,14 +142,17 @@ def generate_new_inventory():
     and send it to the google sheets updater
     """
     pos = 0  # position of item in list
-    for u in USER:
-        g_row = []
-        for i_list in range(len(INVENTORY)-1):
-            g_row.append(INVENTORY[i_list][pos])  # populate the inventory
-        g_row.append(u[-8:])  # add the date field
-        #print(f"This is year {year} and the data is {g_row}")
-        #update_inventory(g_row)
-        pos += 1
+    with open("data.txt", mode = "a") as file:
+        for u in USER:
+            g_row = []
+            for i_list in range(len(INVENTORY)-1):
+                g_row.append(INVENTORY[i_list][pos])  # populate the inventory
+            g_row.append(u[-8:])  # add the date field
+
+            file.write(f"{g_row}\n")
+            #print(f"This is year {year} and the data is {g_row}")
+            #update_inventory(g_row)
+            pos += 1
 
 
 def main():
@@ -158,6 +161,7 @@ def main():
     """
     make_inv_list()
     generate_change_list()
+    simulate_changes()
     generate_new_inventory()
 
 
